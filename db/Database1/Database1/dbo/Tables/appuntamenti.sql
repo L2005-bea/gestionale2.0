44df99555ad468214fc56fde2e3bc525bc8ed887
operativo23 <jacoporotatori@gmail.com>
2024-06-11 15:37:49 +0200

    db8

--- db/Database1/Database1/dbo/Tables/appuntamenti.sql
+++ db/Database1/Database1/dbo/Tables/appuntamenti.sql
@@ -1,7 +1,12 @@
 ﻿CREATE TABLE [dbo].[Appuntamenti]
 (
 	[Id] INT identity  PRIMARY KEY NOT NULL, 
-    [clienteID] INT foreign key (Id) references clienti(Id) NOT NULL, 
+  [clienteID]     int  NOT NULL, 
     [appuntamento_Data_tempo]  DATETIME NOT NULL, 
-    [parrucchieraID]fk_parrucchiera INT foreign key (Id) references parrucchiera(Id) NOT NULL
+    [parrucchieraID] INT foreign key (Id) references parrucchiera(Id) NOT NULL,
+   CONSTRAINT FK_appuntamenti_clienteid FOREIGN KEY ([clienteID]) REFERENCES [dbo].[clienti]([Id]),
+   CONSTRAINT FK_appuntamenti_parrucchieraID FOREIGN KEY ([parrucchieraID]) REFERENCES [dbo].[parrucchiera]([Id])
+   
+   
+
 )
--- db/Database1/Database1/dbo/Tables/dettagli appuntamento.sql
+++ db/Database1/Database1/dbo/Tables/dettagli appuntamento.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[dettagli appuntamento]
 (
 	[Id] INT identity NOT NULL PRIMARY KEY, 
-   [servizioID] int foreign key (Id) references servizi(Id) NOT NULL,
+   [servizioID] int  NOT NULL,
 
-    [clienteID] INT foreign key (Id) references clienti(Id) NOT NULL, 
-    [appuntamentoID] INT  foreign key (Id) references appuntamenti(Id) not NULL
+    [clienteID] INT  NOT NULL, 
+    [appuntamentoID] INT  not NULL,
+    CONSTRAINT FK_appuntamentid_clienteID FOREIGN KEY ([clienteID]) REFERENCES [dbo].[clienti]([Id]),
+    CONSTRAINT FK_appuntamentid_servizioID FOREIGN KEY ([servizioID]) REFERENCES [dbo].[servizi]([Id]),
+    CONSTRAINT FK_appuntamentid_appuntamentoID FOREIGN KEY ([appuntamentoID]) REFERENCES [dbo].[appuntamenti]([Id])
 )
 --foreign key
