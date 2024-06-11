a5a04be752a4b83c270472e0b4e7ce776c55260d
operativo23 <jacoporotatori@gmail.com>
2024-06-11 12:09:38 +0200

    db5

--- db/Database1/Database1/dbo/Tables/appuntamento.sql
+++ db/Database1/Database1/dbo/Tables/appuntamento.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Appuntamenti]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT identity NOT NULL PRIMARY KEY, 
     [clienteID] INT NOT NULL, 
     [appuntamento_Data_tempo] DATETIME NOT NULL
 )
--- db/Database1/Database1/dbo/Tables/dettagli appuntamento.sql
+++ db/Database1/Database1/dbo/Tables/dettagli appuntamento.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[dettagli appuntamento]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT identity NOT NULL PRIMARY KEY, 
     [appuntamentID] INT NOT NULL, 
     [servizioID] INT NOT NULL, 
     [clienteID] INT NOT NULL
