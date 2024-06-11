d90bd5f33c7ef76a66a784524318be19834838ce
operativo23 <jacoporotatori@gmail.com>
2024-06-11 14:59:23 +0200

    db7

--- db/Database1/Database1/dbo/Tables/appuntamenti.sql
+++ db/Database1/Database1/dbo/Tables/appuntamenti.sql
@@ -2,6 +2,6 @@
 (
 	[Id] INT identity  PRIMARY KEY NOT NULL, 
     [clienteID] INT foreign key (Id) references clienti(Id) NOT NULL, 
-    [appuntamento_Data_tempo] DATETIME NOT NULL, 
-    [parrucchieraID] INT foreign key (Id) references parrucchiera(Id) NOT NULL
+    [appuntamento_Data_tempo]  DATETIME NOT NULL, 
+    [parrucchieraID]fk_parrucchiera INT foreign key (Id) references parrucchiera(Id) NOT NULL
 )
