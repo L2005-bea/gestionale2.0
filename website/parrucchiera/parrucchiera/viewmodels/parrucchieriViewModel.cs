be527c129251be78ea4f94b2e2a8e5c495f2ad36
operativo23 <jacoporotatori@gmail.com>
2024-06-12 14:52:59 +0200

    html view parr

---
+++ website/parrucchiera/parrucchiera/viewmodels/parrucchieriViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Parruchiera.Models;
+namespace Parruchiera.viewmodels
+{
+	public class parrucchieraViewModels
+	{
+		public List<paruchiera> parrucchiera { get; set; }
+
+		public parrucchieraViewModels(List<paruchiera> parrucchiera)
+		{
+			this.parrucchiera = parrucchiera;
+		}
+	}
+}
