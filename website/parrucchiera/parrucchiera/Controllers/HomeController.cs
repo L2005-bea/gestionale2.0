f534f8a4e4261940277ada9c380334aefc9a1ba1
betty.lipciu@gmail.com <betty.lipciu@gmail.com>
2024-06-17 16:02:46 +0200

    ster3

--- website/parrucchiera/parrucchiera/Controllers/HomeController.cs
+++ website/parrucchiera/parrucchiera/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using parrucchiera.Data;
 using System.Diagnostics;
 using System.Data.SqlClient;
 
-
 namespace parrucchiera.Controllers
 {
     public class HomeController : Controller
@@ -27,28 +26,39 @@ namespace parrucchiera.Controllers
             return View();
         }
 
-		public IActionResult Servizi()
-		{
-			List<servizi> lista_di_servizi = new List<servizi>();
-			SQLData db = new SQLData();
-			lista_di_servizi = db.GetCaricaServizi();
-			return View(new serviziViewModels(lista_di_servizi));
-		}
-		public IActionResult appuntamento()
-		{
+        public IActionResult Servizi()
+        {
+            List<servizi> lista_di_servizi = new List<servizi>();
+            SQLData db = new SQLData();
+            lista_di_servizi = db.GetCaricaServizi();
+            return View(new serviziViewModels(lista_di_servizi));
+        }
+        public IActionResult appuntamento()
+        {
             List<parrucchieri> lista_di_parrucchieri = new List<parrucchieri>();
             SQLData db = new SQLData();
             lista_di_parrucchieri = db.GetCaricaParrucchieri();
             return View(new parrucchieriViewModels(lista_di_parrucchieri));
-		}
-      
+        }
+
+        [HttpPost]
+        public IActionResult appuntamento(parrucchieri parrucchiere)
+        {
+            SQLData db = new SQLData();
+
+            db.CreaParrucchiere(parrucchiere);
+            List<parrucchieri> lista_di_parrucchieri = new List<parrucchieri>();
+
+            lista_di_parrucchieri = db.GetCaricaParrucchieri();
+            return View(new parrucchieriViewModels(lista_di_parrucchieri));
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-     
-            }
-        }
-   
+
+    }
+}
